9d2cdb74662d2425fef581558a01c4b754b78244
sravanrekandar <sravanrekandar@gmail.com>
2014-04-24 15:22:37 +0530

    fixed jQuery path with bower components

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
         basePath: '../',
 
         files: [
-            'app/lib/jquery.js',
+            'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-route/angular-route.js',
             'bower_components/angular-resource/angular-resource.js',
